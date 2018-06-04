6fc0c748caa3f96e3da83f0f8fd0464603d1dc91
Waldirio M Pinheiro <wpinheir@iroman.home>
2018-06-03 20:52:08 -0700

    github url added

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small calc example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/example-project",
+    url="https://github.com/waldirio/calc",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
